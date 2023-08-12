0beca57e810a517b8073ca2ae943a06ef90dd0da
Jérémy MARIETTE <j-mariette@protonmail.com>
2023-08-12 11:29:15 +0200

    ajout projet vigitek

--- index.php
+++ index.php
@@ -44,7 +44,7 @@
 
 
 
-    <nav class="navbar navbar-expand-md  fixed-top bg-light ">
+    <nav class="navbar navbar-expand-md  fixed-top bg-light">
         <div class="container-fluid">
             <a class="navbar-brand" href="#">Jérémy MARIETTE</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
@@ -90,7 +90,7 @@
                 <p class="questions">Qui suis-je ?</p>
                 <h2 id="titre">A PROPOS</h2>
                 <p>
-                    Développeur web full stack. Je conçois des applications web et mobile. Je travaille quotidiennement avec des langages tels que : HTML, CSS, Javascript et PHP. Vous êtes intéressé) par mon profil ? Contactez-moi via le formulaire en bas de page.
+                    Développeur web full stack. Je conçois des applications web et mobile. Je travaille quotidiennement avec des langages tels que : HTML, CSS, Javascript, PHP et le framework Symfony . Vous êtes intéressé) par mon profil ? Contactez-moi via le formulaire en bas de page.
                 </p>
                 <a href="ressources/jeremy-mariette-cv.pdf"  download>
                     <button id="cv">Voir mon CV</button>
@@ -169,26 +169,22 @@
         <div class="container " id="projets">
             <div class="row ">
                 <h2 class="titre ">PROJETS</h2>
-                <div class="col-sm-12 col-md-4 col-lg-4 ">
-                    <img class="card-img-top " src="ressources/teddy/teddy_bear_size.jpeg.jpg" alt="Card image cap ">
-                    <div class="card-body ">
-                        <h2>Blender 3D</h2>
-                        <p class="card-text ">Modélisation de Teedy Bear en 3D avec le logiciel blender, j'y ai ajouté les textures et couleurs
-                            ainsi qu'un décors pour créer une scène sur le thème de noël.</p>
-                        <a href="ressources/teddy/teddy_bear.blend" class="btn btn-primary ">Voir le projet</a>
-                    </div>
 
-                </div>
                 <div class="col-sm-12 col-md-4 col-lg-4 ">
-                    <img class="card-img-top " src="ressources/unity/jeu-unity.png" alt="Card image cap ">
+                    <img class="card-img-top " src="ressources/dashboard.png" alt="Card image cap ">
                     <div class="card-body ">
-                        <h2>Unity 3D</h2>
-                        <p class="card-text ">Mini jeu vidéo, réalisé avec Unity. Vous incarnez un avatar qui doit collecter des diamant avant d'âtre touché par un ennemi. Ce projet comporte un système de déplacement dans un environnement 3D, un système de collisions, un timer et un compteur de points. 
+                        <h2>PWA Vigitek</h2>
+                        <p class="card-text ">Système de gestion dédié aux entreprises de sécurité.
+                        (Prise et fin de service, Main courante électronique, Edition de rapports, Création de consignes, Messagerie interne)</br>
+                        <small><b>Projet développé avec Symfony 6</b></small>
                         </p>
-                        <a href="http://jmariette.alwaysdata.net/unity/" target="_blank" class="btn btn-primary ">Voir le projet</a>
+                        <a href="https://vigitek.fr/" target="_blank" class="btn btn-primary ">Voir le projet</a>
+
+                        <a href="https://youtu.be/Wm_pZRckWAU" target="_blank" class="btn btn-success">Présentation vidéo</a>
                     </div>
 
                 </div>
+
                 <div class="col-sm-12 col-md-4 col-lg-4 ">
                     <img class="card-img-top " src="ressources/imc.png" alt="Card image cap ">
                     <div class="card-body ">
@@ -200,6 +196,20 @@
                     </div>
 
                 </div>
+
+                <div class="col-sm-12 col-md-4 col-lg-4 ">
+                    <img class="card-img-top " src="ressources/gestion.png" alt="Card image cap ">
+                    <div class="card-body ">
+                        <h2>Application de gestion</h2>
+                        <p class="card-text ">Application de gestion de budget, réalisée en PHP avec une base données et système de requêtes SQL, avec personnalisation du formulaire.
+                        </p>
+                        <a href="http://jmariette.alwaysdata.net/gestion" target="_blank" class="btn btn-primary ">Voir le projet</a>
+                    </div>
+
+                </div>
+             
+              
+             
 
             </div>
         </div>
@@ -218,24 +228,25 @@
                     </div>
 
                 </div>
+
                 <div class="col-sm-12 col-md-4 col-lg-4 ">
-                    <img class="card-img-top " src="ressources/gestion.png" alt="Card image cap ">
+                    <img class="card-img-top " src="ressources/unity/jeu-unity.png" alt="Card image cap ">
                     <div class="card-body ">
-                        <h2>Application de gestion</h2>
-                        <p class="card-text ">Application de gestion de budget, réalisée en PHP avec une base données et système de requêtes SQL, avec personnalisation du formulaire.
+                        <h2>Unity 3D</h2>
+                        <p class="card-text ">Mini jeu vidéo, réalisé avec Unity. Vous incarnez un avatar qui doit collecter des diamant avant d'âtre touché par un ennemi. Ce projet comporte un système de déplacement dans un environnement 3D, un système de collisions, un timer et un compteur de points. 
                         </p>
-                        <a href="http://jmariette.alwaysdata.net/gestion" target="_blank" class="btn btn-primary ">Voir le projet</a>
+                        <a href="http://jmariette.alwaysdata.net/unity/" target="_blank" class="btn btn-primary ">Voir le projet</a>
                     </div>
 
                 </div>
+           
                 <div class="col-sm-12 col-md-4 col-lg-4 ">
-                    <img class="card-img-top " src="images/projet.jpg " alt="Card image cap ">
+                    <img class="card-img-top " src="ressources/teddy/teddy_bear_size.jpeg.jpg" alt="Card image cap ">
                     <div class="card-body ">
-                        <h2>Application Android</h2>
-                        <p class="card-text ">Calculateur d'IMC, réalisé avec le framework IONIC. Ce projet m'a permis de mettre en place un système de manipulation de données (saisie dans le formulaire
-                            et calcul) puis rendu des données avec un système de graphique et de local storage.
-                        </p>
-                        <a href="# " class="btn btn-primary ">Voir le projet</a>
+                        <h2>Blender 3D</h2>
+                        <p class="card-text ">Modélisation de Teedy Bear en 3D avec le logiciel blender, j'y ai ajouté les textures et couleurs
+                            ainsi qu'un décors pour créer une scène sur le thème de noël.</p>
+                        <a href="ressources/teddy/teddy_bear.blend" class="btn btn-primary ">Voir le projet</a>
                     </div>
 
                 </div>
